3d95bf08031c014371dd8d90fef23fda1e8ebf00
Danny Mos <dannymoswow@gmail.com>
2020-06-12 14:32:31 +0200

    Word module e2e test now uses test DB & fixtures

--- test/e2e/word.module.e2e-spec.ts
+++ test/e2e/word.module.e2e-spec.ts
@@ -1,54 +1,39 @@
 import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
-import {INestApplication, ValidationPipe} from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { WordModule } from "../../src/modules/WordModule/WordModule";
-import { getRepositoryToken } from "@nestjs/typeorm";
-import { Word } from "../../src/modules/WordModule/model/entities/Word.entity";
-import { Language } from "../../src/modules/WordModule/model/entities/Language.entity";
-import { WordDto } from "../../src/modules/WordModule/model/dto/Word.dto";
+import {DatabaseModule} from "../../src/modules/DatabaseModule";
+import { TestUtilities } from "./utilities/test.utilities";
 
 describe('Word Module (e2e)', () => {
     let app: INestApplication;
+    let testingUtilities: TestUtilities;
 
     beforeAll(async () => {
-        const mockWordRepo = {
-            save: (word: Word) => {
-                word.id = 1;
-                return word;
-            },
-            create: (dto: WordDto) => {
-                const language = new Language(dto.language, 'testlanguage');
-                return new Word(dto.text, language);
-            }
-        }
-
-        const mockLanguageRepo = {
-            createQueryBuilder: jest.fn(() => ({
-                where: jest.fn(()=> ({
-                    getOne: jest.fn()
-                        .mockImplementation(() => {
-                            return new Language('TE', "testlanguage");
-                        })
-                }))
-            }))
-        }
-
         const moduleFixture = await Test.createTestingModule({
-            imports: [WordModule],
-        }).overrideProvider(getRepositoryToken(Word))
-            .useValue(mockWordRepo)
-            .overrideProvider(getRepositoryToken(Language))
-            .useValue(mockLanguageRepo)
-            .compile();
+            imports: [
+                DatabaseModule,
+                WordModule
+            ],
+            providers: [
+                TestUtilities
+            ]
+        }).compile();
 
         app = moduleFixture.createNestApplication();
         app.useGlobalPipes(new ValidationPipe());
         await app.init();
+
+        testingUtilities = app.get<TestUtilities>(TestUtilities);
     });
+
+    beforeEach(async () => {
+        await testingUtilities.loadFixtures();
+    })
 
     it('submit new word to /word should return 2xx (POST)', () => {
         const mockRequest = {
-            text: "aword",
+            text: "testt",
             language: "TE"
         }
 
@@ -56,7 +41,7 @@ describe('Word Module (e2e)', () => {
             .post('/word')
             .send(mockRequest)
             .expect(201)
-            .expect('{"id":1,"text":"aword","language":{"slug":{"slug":"TE","name":"testlanguage"},"name":"testlanguage"}}');
+            .expect('{"id":1,"text":"testt","language":{"id":1,"slug":"TE","name":"testlanguage"}}');
     });
 
     it('submit new INVALID word to /word should return 4xx (POST)', () => {
