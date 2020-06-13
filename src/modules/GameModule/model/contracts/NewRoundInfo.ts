a624f9a4dc6880d7af7bb1fa516d1674e2e1c8ea
Danny Mos <dannymoswow@gmail.com>
2020-06-13 18:59:39 +0200

    Removed useless getters & setters

--- src/modules/GameModule/model/contracts/NewRoundInfo.ts
+++ src/modules/GameModule/model/contracts/NewRoundInfo.ts
@@ -6,31 +6,9 @@ export class NewRoundInfo {
         this._roundNumber = roundNumber;
     }
 
-    private _wordLength: number;
+    public _wordLength: number;
 
-    private _firstCharacter: string;
+    public _firstCharacter: string;
 
-    private _roundNumber: number;
-
-    get roundNumber(): number {
-        return this._roundNumber;
-    }
-
-    set roundNumber(value: number) {
-        this._roundNumber = value;
-    }
-    get firstCharacter(): string {
-        return this._firstCharacter;
-    }
-
-    set firstCharacter(value: string) {
-        this._firstCharacter = value;
-    }
-    get wordLength(): number {
-        return this._wordLength;
-    }
-
-    set wordLength(value: number) {
-        this._wordLength = value;
-    }
+    public _roundNumber: number;
 }
