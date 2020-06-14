7b9c37acf030ab5205685b2373d6f41b281a1435
Danny Mos <dannymoswow@gmail.com>
2020-06-14 22:56:46 +0200

    Removed swagger, since it is not used

--- src/Main.ts
+++ src/Main.ts
@@ -1,21 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from "@nestjs/common";
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './modules/AppModule';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const options = new DocumentBuilder()
-      .setTitle('Word API')
-      .setDescription('API used to register new words')
-      .setVersion('1.0')
-      .addTag('Words')
-      .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
-
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
